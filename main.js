71ca42ba0fd984b455a2d424776a4312fd81f68a
ulldecorb <ausias.bertran@gmail.com>
2021-06-27 04:22:08 +0200

    Add javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+const landing = document.querySelector('.landing');
+const button = document.querySelector("button");
+
+
+
+
+
+button.addEventListener("click", hidelanding(event));
+
+function hidelanding(event) {
+    Event.stop(event);
+    landing.style.display = 'none';
+    alert('hey'); 
+}
