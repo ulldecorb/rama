5f9f13a6d122c95e3019a247b41b5064168262dd
ulldecorb <ausias.bertran@gmail.com>
2021-09-12 04:20:26 +0200

    Add torch basic logic

--- html-cuts/001intro/main.js
+++ html-cuts/001intro/main.js
@@ -1,6 +1,10 @@
-var siguerratones = document.getElementById('siguerratones').style;
+const torch = document.getElementById('torch').style;
+const torchImage = document.getElementById('torch-image').style;
 
-function seguir() {
-  siguerratones.top = parseInt(window.event.pageY - 5 ) + "px";
-  siguerratones.left = parseInt(window.event.pageX - 35) + "px";
+function followTorch() {
+  torch.top = parseInt(window.event.pageY - 5 ) + "px";
+  torch.left = parseInt(window.event.pageX - 35) + "px";
+  // torchImage.top = parseInt(window.event.pageY + 200) + "px";
+  // torchImage.left = parseInt(window.event.pageX + 200) + "px";
+
 }
