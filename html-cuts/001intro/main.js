68c8a36581658c519c68e8c68781f02b5449bca2
ulldecorb <ausias.bertran@gmail.com>
2021-09-12 02:34:20 +0200

    Add flashlight effect

--- html-cuts/001intro/main.js
+++ html-cuts/001intro/main.js
@@ -1,6 +1,6 @@
 var siguerratones = document.getElementById('siguerratones').style;
 
 function seguir() {
-  siguerratones.top = parseInt(window.event.pageY - 300) + "px";
-  siguerratones.left = parseInt(window.event.pageX - 300) + "px";
+  siguerratones.top = parseInt(window.event.pageY - 5 ) + "px";
+  siguerratones.left = parseInt(window.event.pageX - 35) + "px";
 }
