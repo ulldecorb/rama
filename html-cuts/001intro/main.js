e5b38f85e59e7c790a6d0aa4ec86a7fef0e39c12
ulldecorb <ausias.bertran@gmail.com>
2021-09-13 11:57:50 +0200

    Place the items in the astronaut sticker

--- html-cuts/001intro/main.js
+++ html-cuts/001intro/main.js
@@ -1,10 +1,11 @@
 const torch = document.getElementById('torch').style;
 const torchImage = document.getElementById('torch-image').style;
+const torchStrip = document.getElementById('torch-strip').style
 
 function followTorch() {
   torch.top = parseInt(window.event.pageY - 5 ) + "px";
   torch.left = parseInt(window.event.pageX - 35) + "px";
-  // torchImage.top = parseInt(window.event.pageY + 200) + "px";
-  // torchImage.left = parseInt(window.event.pageX + 200) + "px";
+  torchImage.top = torchStrip.event.pageY;
+  torchImage.left = torchStrip.event.pageX;
 
 }
